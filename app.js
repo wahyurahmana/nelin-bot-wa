a3d8fc06ceb16d43f1501cefe33e2caaa36d745f
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-06-23 18:39:10 +0800

    fitur session

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const qrcode = require('qrcode-terminal');
 const axios = require('axios')
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const {
+    Client,
+    LegacySessionAuth
+} = require('whatsapp-web.js');
 
 //scan qr code for login
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {
+        small: true
+    });
+});
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
+const client = new Client({
+    authStrategy: new LegacySessionAuth({
+        session: sessionData
+    })
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
 //client sudah login
@@ -15,21 +45,17 @@ client.on('ready', () => {
 
 //menerima inputan
 client.on('message', message => {
-    if(message.body === '-help') {
-
-message.reply(`Hai, Saya Nelin.
-Ada Yang Bisa Nelin Bantu ?
-Nelin Menerima Perintah : 
-*-cuaca <nama_daerah>*`);
-
-    }else if (message.body === '-cuaca list-nama-daerah'){
-message.reply(`Ini Nama Daerah Yang Bisa Dicek Cuacanya
-*sumbawa*
-`)
-    }else if (message.body === '-cuaca sumbawa'){
+    if (message.body === '-help') {
+    message.reply('Hai, Saya Nelin.'+ "\n" +
+    'Ada Yang Bisa Nelin Bantu ?Nelin Menerima Perintah :'+ "\n" +
+    '*-cuaca <nama_daerah>*');
+    } else if (message.body === '-cuaca list-nama-daerah') {
+        message.reply('Ini Nama Daerah Yang Bisa Dicek Cuacanya'+ "\n" +
+        '*sumbawa*');
+    } else if (message.body === '-cuaca sumbawa') {
         axios({
-            url : `http://dataservice.accuweather.com/currentconditions/v1/205207?apikey=${process.env.API_KEY_ACCUWEATHER}&language=id-id`
-        })
+                url: `http://dataservice.accuweather.com/currentconditions/v1/205207?apikey=${process.env.API_KEY_ACCUWEATHER}&language=id-id`
+            })
             .then((result) => {
                 console.log
                 message.reply(`Nelin Mendapatkan Informasi Bahwa Cuacanya ${result.data[0].WeatherText} Dengan Temperature ${result.data[0].Temperature.Metric.Value}${result.data[0].Temperature.Metric.Unit}`)
@@ -38,9 +64,7 @@ message.reply(`Ini Nama Daerah Yang Bisa Dicek Cuacanya
                 console.log(err)
                 message.reply('maaf nelin tidak mengerti')
             })
-    }
-
-    else{
+    } else {
         message.reply('maaf nelin tidak mengerti')
     }
 });
